cdaab635d889569e99b6076177455d845f56fc62
William Thomson <william.thomson@financial-cloud.com>
2019-08-02 16:58:17 +0100

    PokeAPI - Styling Update
    Updated the UI slightly to include a hero image header. For now my main focus to get a decent looking interface before i add anymore functionality.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokeListComponent } from './components/poke-list/poke-list.component';
-import { PokeManComponent } from './components/poke-man/poke-man.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PokeListComponent,
-    PokeManComponent,
     HeaderComponent
   ],
   imports: [
